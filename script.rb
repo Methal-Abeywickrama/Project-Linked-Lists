138fc074782c0c137d736207237d9e2512f572d7
Methal Abeywickrama <methal8779@gmail.com>
2021-08-14 22:00:45 +0530

    done ... maybe?

--- script.rb
+++ script.rb
@@ -11,6 +11,60 @@ class LinkedList
     @list.push(new_node)
     @list.last.next_node = @list.index(new_node)
   end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    @list.shift(new_node)
+    @list[0].next_node = 1
+  end
+
+  def size
+    @list.length
+  end
+
+  def head
+    @list[0]
+  end
+
+  def tail
+    @list.last
+  end
+
+  def at(index)
+    @list[index]
+  end
+
+  def pop
+    @list.pop
+  end
+
+  def contains?(value)
+    @list.any? { |n| n.value == value }
+  end
+
+  def find(value)
+    @list.each_with_index { |n, i| return i if n.value == value }
+  end
+
+  def to_s
+    @list.each { |i| print "( #{i} ) -> " }
+    print 'nil'
+  end
+
+  def insert_at(value, index)
+    front_arr = @list[0..index - 1]
+    back_arr = @list[index + 1..]
+    back_arr.each { |v| v.next_node += 1 }
+    front_arr.push(Node.new(value, index))
+    @list = front_arr + back_arr
+  end
+
+  def remove_at(index)
+    front_arr = @list[0..index - 1]
+    back_arr = @list[index + 1..]
+    back_arr.each { |v| v.next_node -= 1 }
+    @list = front_arr + back_arr
+  end
 end
 
 # the class for the node
